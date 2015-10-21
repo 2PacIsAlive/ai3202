765012c6873c28bd5525398f9c82bb88de60f2bd
Jared Jolton <jared.jolton@gmail.com>
2015-10-20 23:40:19 -0600

    command line arg parsing kindof sortof implemented

---
+++ Jolton_CSCI_Assignment6/bayesnet.py
@@ -0,0 +1,40 @@
+import argparse
+
+def getArgs():
+	parser = argparse.ArgumentParser(description='Specify program behavior.')
+	#parser.add_argument('-g', action="store_true", default=False)
+	parser.add_argument('-g', action="store", dest="conditional_probability")
+	parser.add_argument('-j', action="store", dest="joint_probability")
+	parser.add_argument('-m', action="store", dest="marginal_probability")
+	parser.add_argument('-p', action="store", dest="set_prior")
+	args=parser.parse_args()
+	return args
+
+def performAction(arguments):
+	if arguments.conditional_probability != None:
+		conditionalProbability(arguments.conditional_probability)
+	if arguments.joint_probability != None:
+		jointProbability(arguments.joint_probability)
+	if arguments.marginal_probability != None:
+		marginalProbability(arguments.marginal_probability)
+	if arguments.set_prior != None:
+		setPrior(arguments.set_prior)
+
+def conditionalProbability(args):
+	print 'do cond prob on:', args
+
+def jointProbability(args):
+	print 'do joint prob on:', args
+
+def marginalProbability(args):
+	print 'do marg prob on:', args
+
+def setPrior(args):
+	print 'set prior for:', args
+
+
+if __name__=="__main__":
+	performAction(getArgs())
+
+	
+
