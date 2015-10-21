c284297e6f65bb688da52691d1bbe11d397de2b6
Jared Jolton <jared.jolton@gmail.com>
2015-10-21 15:36:41 -0600

    added graph

--- Jolton_CSCI_Assignment6/bayesnet.py
+++ Jolton_CSCI_Assignment6/bayesnet.py
@@ -1,5 +1,31 @@
 import argparse
 
+class GRAPH():
+    def __init__(self):
+        #instantiate dictionary
+        self.graph = {}
+    def addVertex(self, value):
+        if value in self.graph:
+            print 'Vertex already exists.'
+        else:
+            #add the vertex with an empty list to store connections
+            self.graph.update({value: []})
+    def addEdge(self, value1, value2):
+        if value1 and value2 in self.graph:
+            #add both nodes to connection lists to ensure unweighted, bidirectional graph
+            self.graph[value1].append(value2)
+            self.graph[value2].append(value1)
+        else:
+            print 'One or more vertices not found.'
+    def findVertex(self, value):
+        if value in self.graph:
+            #keys() function used to access all vertices in graph
+            for vertex in self.graph.keys():
+                if vertex == value:
+                    print 'Found vertex:', vertex, 'and shared edge(s) with:', self.graph[vertex]
+        else: 
+            print 'Vertex not found.'
+
 def getArgs():
 	parser = argparse.ArgumentParser(description='Specify program behavior.')
 	#parser.add_argument('-g', action="store_true", default=False)
